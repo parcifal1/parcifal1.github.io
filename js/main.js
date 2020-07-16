ba4e40a083cacafddc3a8624c7a75b44e6fe56c1
parcifal1 <60840448+parcifal1@users.noreply.github.com>
2020-07-16 15:57:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+     nextEl: '.series-navigation_arrow',
+    },
+    navigation: {
+        nextEl: '.series-navigation_arrow',
+       },
+       breakpoints: {
+           // when window width is => 996px
+           540: {
+               slidesPerView: 2,
+           },
+       }
+});
+
+ var menuButton = document.querySelector('.menu-btn');
+ var menu = document.querySelector('.header');
+ menuButton.addEventListener('click', function(){
+    menuButton.classList.toggle('menu-btn-active');
+     menu.classList.toggle('header-active');
+ })
